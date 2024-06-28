c686cbe396a334847f0e81fc4f1ab749f2a948ee
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-06-28 09:46:24 +0000

    set new security key

--- librarychat/settings.py
+++ librarychat/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j76opzg8dpj*%d1a=*r!ywr6bsybda*nh+nm1t0f!c6xhu4#ie'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
