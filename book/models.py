840e467af71fc8e2caef148b8e3e4f7384c86dda
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-07-01 14:47:20 +0000

    add method and Meta class in Review model

--- book/models.py
+++ book/models.py
@@ -9,12 +9,20 @@ class Bibliography(models.Model):
     publisher = models.CharField(max_length=100)
     year = models.IntegerField()
     edition = models.IntegerField(blank=True, null=True)
+
     
 class Review(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews", null=True)
     bibliography = models.ForeignKey(Bibliography, on_delete=models.CASCADE, related_name="reviews", blank=True, null=True)
+
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Review for {self.bibliography.title} created on {self.created_on}"
 
 
     
