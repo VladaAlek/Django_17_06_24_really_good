084333f4eb6f8baf173f7be14d16e0f1ce58053c
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-07-01 12:39:53 +0000

    refactore Bibliography and Review models

--- book/models.py
+++ book/models.py
@@ -2,18 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Review(models.Model):
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True, null=True)
-
-    
 class Bibliography(models.Model):
+    reader = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reader_account", null=True)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=150)
     publisher = models.CharField(max_length=100)
     year = models.IntegerField()
     edition = models.IntegerField(blank=True, null=True)
-    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="books", blank=True, null=True)
+    
+class Review(models.Model):
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews", null=True)
+    bibliography = models.ForeignKey(Bibliography, on_delete=models.CASCADE, related_name="reviews", blank=True, null=True)
+
+
+    
+
+    
 
 
 
