0797236ccd9f4bef82fcaba0e666c897ca462539
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-07-19 14:52:48 +0000

    style: add classes to bootstrap

--- librarychat/settings.py
+++ librarychat/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-vladaalek-django170624r-v5jh3fd2oy3.ws.codeinstitute-ide.net',
 '.herokuapp.com']
 
