6604c0310cdb813bfc4dfc4ab8fab89d0499bf8e
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-07-20 10:36:58 +0000

    add path in urls.py and function in views.py for about.html templ

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
+    path('about/', views.about, name='about'),
 ]
--- book/views.py
+++ book/views.py
@@ -8,3 +8,6 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.all()
     template_name = "book/index.html"
     paginate_by = 6
+
+def about(request):
+    return render(request, 'book/about.html')
