9f873e0633105da0eadab9567143abbff4127f8f
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-08-04 12:22:18 +0000

    feat: add js code

---
+++ static/js/reviews.js
@@ -0,0 +1,24 @@
+const editButtons = document.getElementsByClassName("btn-edit");
+const commentText = document.getElementById("id_body");
+const commentForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+
+/**
+* Initializes edit functionality for the provided edit buttons.
+* 
+* For each button in the `editButtons` collection:
+* - Retrieves the associated review's ID upon click.
+* - Fetches the content of the corresponding review.
+* - Populates the `commentText` input/textarea with the review's content for editing.
+* - Updates the submit button's text to "Update".
+* - Sets the form's action attribute to the `edit_review/{reviewId}` endpoint.
+*/
+for (let button of editButtons) {
+  button.addEventListener("click", (e) => {
+    let reviewId = e.target.getAttribute("review_id");
+    let reviewContent = document.getElementById(`review${reviewId}`).innerText;
+    commentText.value = reviewContent;
+    submitButton.innerText = "Update";
+    commentForm.setAttribute("action", `edit_review/${reviewId}`);
+  });
+}
